0daa8d1b983143010857550c358b1245b7304509
Deiniol Owen <dowen92@hotmail.co.uk>
2018-08-20 14:00:14 +0200

    Added project

---
+++ SendingEmail/Assets/_Scripts/EmailManager.cs
@@ -0,0 +1,16 @@
+﻿/***********************************************************
+#Script Name:	EmailManager
+#Author:		Deiniol Owen
+#GitHub:		https://github.com/Dowen92?tab=repositories
+#Blog:			https://dowen92dev.wordpress.com/
+***********************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmailManager : MonoBehaviour {
+
+
+
+}
